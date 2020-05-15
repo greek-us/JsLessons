4ca0ece7992fac336a5cb3615c1abbf86de6b142
greek-us <sergy.job@yandex.ru>
2020-05-15 23:51:24 +0300

    Циклы (lesson#1)

--- main.js
+++ main.js
@@ -50,3 +50,41 @@ document.querySelector('.f-2').onclick = () => {
   console.log('===');
 }
 //************************************
+
+//Задачи циклы и фунцкии
+//вывести четные числа
+
+//   for (var i = 100; i > 1; i--) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//  }
+// }
+// //заполнить массив x
+// let massiv = [];
+// for (var i = 0; i < 10; i++) {
+//   massiv.push('x');
+// }
+// console.log(massiv);
+//заполнить от 1 до 10
+// let massiv=[];
+// for (var i = 1; i < 11; i++) {
+//   massiv.push(i);
+// }
+// console.log(massiv);
+//заполнить массив случайными числами(дробями) от 0 до 1
+// let massiv = [];
+//  for (var i = 0; i < 10; i++) {
+//     massiv.push(Math.random().toFixed(2));
+//  }
+//  console.log(massiv);
+//заполнить массив случайными числами  от 0 до 10
+// let massiv = [];
+// for (var i=0; i<10; i++) {
+//   massiv.push(Math.floor(Math.random()*10));
+// }
+// console.log(massiv);
+//Вывести только четные
+let massiv = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (var i = 0; i<10; i++) {
+  console.log(massiv);
+}
