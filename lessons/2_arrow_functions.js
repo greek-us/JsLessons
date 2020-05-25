7973c0dba6db72b390eadbb83fbdad50a3e9fb63
greek-us <sergy.job@yandex.ru>
2020-05-25 22:56:18 +0300

    25.05.20

--- lessons/2_arrow_functions.js
+++ lessons/2_arrow_functions.js
@@ -1,7 +1,16 @@
-function sum(a + b) {
-  return a + b;
-}
+// function sum(a + b) {
+//   return a + b;
+// }
+//
+// function cub(a) {
+//   return a ** 3
+// }
+// -----------------------------------
+// const sum = (a, b) => {
+//   return a + b;
+// }
+// console.log(sum(41, 1));
 
-function cub(a) {
-  return a ** 3
-}
+
+const cub = (c,d) => c ** 3/d;
+console.log(cub(2, 4));
